e85d83a669ae7b24539e22afe9b575c944511a87
feehx25 <49326819+feehx25@users.noreply.github.com>
2021-03-31 21:35:13 -0300

    Subo los ejercicios 1 y 2 terminados (creo)

---
+++ ej1.py
@@ -0,0 +1,7 @@
+
+nombre= input("Ingresa tu nombre ")
+print ("hola "+ nombre)
+num1= int(input("Ingrese el primer factor "))
+num2= int(input("Ingrese el segundo factor "))
+prod = num1*num2
+print ("el producto es ", prod)
---
+++ ej2.py
@@ -0,0 +1,94 @@
+def calcularPerimetroR(base, altura):
+    per=(base)*2+(altura)*2
+    return "{0:.2f}".format(per)
+
+def calcularAreaR(base,altura):
+    area= base*altura
+    return "{0:.2f}".format(area)
+
+def calcularArea2(x1,x2,y1,y2):
+    area2=(x2-x1)*(y2-y1)
+    return "{0:.2f}".format(area2)
+
+def calcularPerimetroC(radio):
+    perimetro=radio * 2 * 3.14
+    return "{0:.2f}".format(perimetro)
+
+def calcularAreaC(radio):
+    area= radio**2 * 3.14
+    return "{0:.2f}".format(area) 
+
+def calcularVolumen(radio):
+    V = 4/3 * 3.14 * radio**3
+    return "{0:.2f}".format(V)
+
+def calcularHipotenusa(cateto1,cateto2):
+    h =(cateto1**2 + cateto2**2)**0.5
+    return "{0:.2f}".format(h)
+
+calculo=input("""Elija su calculo
+1.Calcular perimetro
+2.Calcular area
+3.Calcular area dadas sus coordenadas
+4.Calcular el perimetro de un circulo
+5.Calcular el area de un circulo
+6.Calcular el volumen de una esfera
+7.Calcular la hipotenusa
+""")
+
+if calculo.isdigit():
+    if calculo == "1" :
+        a=int(input("Ingrese la altura "))
+        b=int(input("Ingrese la base "))
+        if a < 1 or b < 1:
+            print("No ingrese numeros negativos")
+        else:
+            print("El perimetro es ",calcularPerimetroR(a,b))
+    if calculo == "2":
+        a=int(input("Ingrese la altura "))
+        b=int(input("Ingrese la base "))
+        if a < 1 or b < 1:
+             print("No ingrese numeros negativos")
+        else:
+             print("El area es ", calcularAreaR(a,b))
+    if calculo == "3":
+        x1=int(input("Ingrese x1 "))
+        x2=int(input("Ingrese x2 "))
+        y1=int(input("Ingrese y1 "))
+        y2=int(input("Ingrese y2 "))
+        if x1 < 1 or x2 < 1 or  y1 < 1 or y2 < 1:
+             print("No ingrese numeros negativos")
+        else:
+            print ("El area es ", calcularArea2(x1,x2,y1,y2))
+    if calculo == "4" :
+        r=int(input("Ingrese el radio "))
+        if r < 1:
+            print("No ingrese numeros negativos")
+        else:
+            print("El perimetro es ",calcularPerimetroC(r))
+    if calculo == "5" :
+        r=int(input("Ingrese el radio "))
+        if r<1:
+            print("No ingrese numeros negativos")
+        else:
+            print("El perimetro es ",calcularAreaC(r))
+    if calculo == "6" :
+        r=int(input("Ingrese el radio "))
+        if r<1:
+            print("No ingrese numeros negativos")
+        else:
+            print("El volumen es ",calcularVolumen(r))
+    if calculo == "7":
+        a=int(input("Ingrese un cateto "))
+        b=int(input("Ingrese el otro cateto "))
+        if a < 1 or b < 1:
+             print("No ingrese numeros negativos")
+        else:
+             print("La hipotenusa es ", calcularHipotenusa(a,b))
+    if int(calculo) <1 or int(calculo) >7:
+        print("Ingrese numero dentro de las opciones")
+else :
+    print("Ingrese un numero")
+
+
+
