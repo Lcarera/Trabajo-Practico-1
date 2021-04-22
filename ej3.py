0e74f67af55379545edb4558b0bc3851ff0d0a91
luciano <lcarera@alumno.huergo.edu.ar>
2021-04-22 17:03:13 -0300

    Agrego validacion de inputs

--- ej3.py
+++ ej3.py
@@ -1,7 +1,7 @@
 """El programa pide un numero y haciendo uso de la recursividad devuelve el factorial"""
 def fact(n):
- """La variable se llama a si misma hasta que el valor de "n" sea 0
- despues suma todos los resultados que se van almacenando."""
+    """La variable se llama a si misma hasta que el valor de "n" sea 0
+    despues suma todos los resultados que se van almacenando."""
 
     if n == 0:
         f = 1
@@ -14,7 +14,7 @@ factorial= int(input("Ingrese un numero entero y se va a devolver su factorial: 
 if factorial < 0:
     print("No se puede calcular el factorial de un numero negativo.")
 else:
-    print(f"El factorial de {factorial} es {fact(factorial)}"")
+    print(f"El factorial de {factorial} es {fact(factorial)}")
    
 
         
