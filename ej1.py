c140e8a122ca1beecbab95e21ca1eef9e2870cb5
luciano <lcarera@alumno.huergo.edu.ar>
2021-04-22 16:58:17 -0300

    Agrego validacion de inputs

--- ej1.py
+++ ej1.py
@@ -1,8 +1,21 @@
-""" El programa pide el nombre y devuelve un saludo.
-Despues pide dos numeros y hace una multiplicacion."""
-nombre= input("Ingresa tu nombre: ")
-print (f"Hola {nombre}!")
-num1= int(input("Ingrese el primer factor: "))
-num2= int(input("Ingrese el segundo factor: "))
-prod = num1*num2
-print ("el producto es: ", prod)
+"""Pide el nombre dek usuario y devuele un saludo,
+despues pide dos factores para hacer un producto"""
+try:
+    nombre = input("Ingrese su nombre completo \n")
+    if nombre.isdigit() == True:
+        print("No ingrese numeros!")
+    else:
+        if nombre == "":
+            print("No ingreso su nombre")
+        else:
+            print(f"Hola {nombre}!")
+except ValueError:
+    print("Nombre no valido")
+
+try:   
+    fact1 = float(input("Ingrese el primer factor: "))
+    fact2 = float(input("Ingrese el segundo factor: "))
+    prod = fact1 * fact2
+    print(f"El producto entre {fact1} y {fact2} es {prod}")
+except ValueError:
+    print("Datos no validos")
