97a8264efdb77965797a791b549f0035a7c00d83
luciano <lcarera@alumno.huergo.edu.ar>
2021-05-05 22:21:24 -0300

    Mejoro el ejercicio 6

--- ej6.py
+++ ej6.py
@@ -5,23 +5,27 @@ def compararMayor(a,b):
         return a
     else:
         return b
+numeros=[]
+try:
+    x1=int(input("ingrese el primer numero: "))
+    x2=int(input("ingrese el segundo numero: "))
+    x3=int(input("ingrese el tercer numero: "))
+    x4=int(input("ingrese el cuarto numero: "))
+    numeros=[x1,x2,x3,x4]
+except ValueError:
+    print("Datos Invalidos")
+productos = []
 
-x1=int(input("ingrese el primer numero: "))
-x2=int(input("ingrese el segundo numero: "))
-x3=int(input("ingrese el tercer numero: "))
-x4=int(input("ingrese el cuarto numero: "))
+for posicion1, valor1 in enumerate(numeros):
+    for posicion2, valor2 in enumerate(numeros):
+        if posicion1 != posicion2:
+            productos.append(valor1 * valor2)
 
+print(productos)
 
-x12= x1 * x2   
-x13= x1 * x3
-x14= x1 * x4   
-x23= x2 * x3
-x24= x2 * x4
-x34= x3 * x4
-
-mayor1= compararMayor(x12,x13)    
-mayor2= compararMayor(mayor1,x14) 
-mayor3= compararMayor(mayor2,x23) 
-mayor4= compararMayor(mayor3,x24)
-Mayor= compararMayor(mayor4,x34) 
-print("El mayor producto es", Mayor)
+# mayor1= compararMayor(x12,x13)    
+# mayor2= compararMayor(mayor1,x14) 
+# mayor3= compararMayor(mayor2,x23) 
+# mayor4= compararMayor(mayor3,x24)
+# Mayor= compararMayor(mayor4,x34) 
+print("El mayor producto es", max(productos))
